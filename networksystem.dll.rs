b7e029c69cc988ab3004e17a7dc7a8e884e1e899
aImLaZy <162299444+aImLaZy@users.noreply.github.com>
2024-03-06 13:13:28 +0100

    Add files via upload

---
+++ networksystem.dll.rs
@@ -0,0 +1,10 @@
+/*
+ * Generated using https://github.com/a2x/cs2-dumper
+ * Wed, 6 Mar 2024 12:09:18 +0000
+ */
+
+#![allow(non_snake_case, non_upper_case_globals)]
+
+pub mod ChangeAccessorFieldPathIndex_t {
+    pub const m_Value: usize = 0x0; // int16_t
+}
